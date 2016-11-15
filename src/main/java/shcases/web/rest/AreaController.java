123cb14908a4b10566288dd47398fa5c83b79d59
Szymon Wolak <szymon.wolak.adm@gmail.com>
2016-11-15 21:15:45 +0100

    Commit 003

--- src/main/java/shcases/web/rest/AreaController.java
+++ src/main/java/shcases/web/rest/AreaController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import shcases.domain.Grodek;
 import shcases.repository.GrodekRepository;
 
 import javax.inject.Inject;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by SzymonW on 2016-11-03.
@@ -18,11 +21,22 @@ public class AreaController {
     @Inject
     GrodekRepository grodekRepository;
 
-    @RequestMapping(value = "{details}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ModelAndView getBlog(ModelAndView mv, @PathVariable String details) {
-        grodekRepository.findOne(details);
-        mv.addObject("details", grodekRepository.findOne(details));
-        mv.setViewName("details");
+    @RequestMapping(value = "/place/{place}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ModelAndView getBlog(ModelAndView mv, @PathVariable String place) {
+
+        List<Grodek> aa = andrzej(place);
+
+        mv.addObject("place", aa);
+        mv.setViewName("area");
         return mv;
+    }
+
+    public List<Grodek> andrzej(String a)
+    {
+        List<Grodek> allGrodekObjects = grodekRepository.findAll();
+
+        return allGrodekObjects.stream()
+                    .filter(s -> s.getPlace().equals(a))
+                    .collect(Collectors.toList());
     }
 }
