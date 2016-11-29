fef76bb55ba3ef4d31f4f1216f94c7ecb9b6fc48
Szymon Wolak <szymon.wolak.adm@gmail.com>
2016-11-29 21:21:29 +0100

    Commit 005

--- src/main/java/shcases/web/rest/AreaController.java
+++ src/main/java/shcases/web/rest/AreaController.java
@@ -1,11 +1,14 @@
 package shcases.web.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import shcases.config.apidoc.SwaggerConfiguration;
 import shcases.domain.Grodek;
 import shcases.repository.GrodekRepository;
 
@@ -21,13 +24,21 @@ public class AreaController {
     @Inject
     GrodekRepository grodekRepository;
 
+    private final Logger log = LoggerFactory.getLogger(AreaController.class);
+
     @RequestMapping(value = "/place/{place}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ModelAndView getBlog(ModelAndView mv, @PathVariable String place) {
 
+        log.warn("WAREN!");
+
         List<Grodek> aa = andrzej(place);
+        String photoName = getPhoto().toLowerCase();
 
         mv.addObject("place", aa);
+        mv.addObject("photoName", place );
         mv.setViewName("area");
+
+
         return mv;
     }
 
@@ -35,8 +46,18 @@ public class AreaController {
     {
         List<Grodek> allGrodekObjects = grodekRepository.findAll();
 
+
         return allGrodekObjects.stream()
                     .filter(s -> s.getPlace().equals(a))
                     .collect(Collectors.toList());
+    }
+
+    public String getPhoto() {
+        List<Grodek> allGrodekObjects = grodekRepository.findAll();
+        String photo = null;
+        for (Grodek g : allGrodekObjects) {
+            photo = g.getPlace();
+        }
+        return photo;
     }
 }
